3a524aea91d1bbbe56592169fc6893b6e762bc5a
= <=>
2025-06-08 12:32:11 +0530

    till env basics

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+const absolutePath = __dirname + '/views/index.html';
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
